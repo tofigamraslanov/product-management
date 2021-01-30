e400d9b8fb05100229f95fce5942618b4e48b9a7
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-01-30 23:58:31 +0400

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // IEntity implement eden class bir database table'dir
+    public interface IEntity
+    {
+
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // Çiplak Class Qalmasin
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
