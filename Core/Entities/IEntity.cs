95793b97c9f21f9983ca1174a92dd6f2fd0e5f55
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-02-07 00:56:53 +0400

    "Core" layer added and some refactoring stuff done.

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     // IEntity implement eden class bir database table'dir
     public interface IEntity
