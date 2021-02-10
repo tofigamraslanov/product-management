3e026dddc19b7ced86eda952d48cf332fc1b2477
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-02-11 01:04:19 +0400

    Results folder added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Product name is invalid";
+        public static string MaintenanceTime = "The system is in maintenance";
+        public static string ProductsListed = "Products listed";
+    }
+}
