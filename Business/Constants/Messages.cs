078b9fba1431c76c212abcdf392192accef9697f
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-02-25 01:08:14 +0400

    Business codes refactored

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,9 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Product name is invalid";
         public static string MaintenanceTime = "The system is in maintenance";
         public static string ProductsListed = "Products listed";
+        public static string ProductCountOfCategoryError= "There must max of 10 product in the category";
+        public static string ProductUpdated = "Product updated";
+        public static string ProductNameAlreadyExists="Product name already exists";
+        public static string CategoryLimitExceeded= "Category limit exceeded";
     }
 }
---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,33 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                    return logic;
+            }
+            return null;
+        }
+
+        //public static List<IResult> Run(params IResult[] logics)
+        //{
+        //    List<IResult> errorResults = new List<IResult>();
+        //    foreach (var logic in logics)
+        //    {
+        //        if (!logic.Success)
+        //        {
+        //            errorResults.Add(logic);
+        //        }
+        //    }
+        //    return errorResults;
+        //}
+    }
+}
